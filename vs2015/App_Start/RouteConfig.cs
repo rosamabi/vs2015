1a93850c7860713dae0ab8d3b7d2d50ba74c9edf
rosamabi <rosa.beatriz@outlook.com>
2016-07-22 15:12:50 -0300

    forum

--- vs2015/App_Start/RouteConfig.cs
+++ vs2015/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace vs2015
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "vs2015.Controllers" }
             );
             
         }
